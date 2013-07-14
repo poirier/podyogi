b0123972495a64dd50b059cf5fa9fd5bcbf7b565
Dan Poirier <dan@poirier.us>
2013-07-14 10:38:25 -0400

    Work on packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+requirements = [
+    # Requests 1.2.1-1.2.3 - won't install under Python 3.3
+    'requests==1.2.0',
+    'feedparser'
+]
 
 setup(
     name='podyogi',
@@ -6,9 +12,9 @@ setup(
     packages=[''],
     url='https://bitbucket.org/poirier/pypodder/',
     license='Apache 2.0',
-    author='poirier',
+    author='Dan Poirier',
     author_email='dan@poirier.us',
-    description='Podcatcher',
-    requires=['requests', 'feedparser']
+    description='Podcatcher for Pythonistas',
+    install_requires=requirements,
+    long_description=open("README.rst").read(),
 )
-FIXME require recent enough 'requests'
